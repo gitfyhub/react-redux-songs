9cc59a23dd39d7b212eee9d9668f778fecbbf44f
Faisal Younus <gitfyhub>
2020-01-20 20:03:28 -0600

    add SongDetail

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 // Action creator
 
-export const selectSong = (song) => {
+export const selectSong = song => {
   // Return an action
   return {
-      type: 'SONG_SELECTED',
-      payload: song
+    type: "SONG_SELECTED",
+    payload: song
   };
 };
